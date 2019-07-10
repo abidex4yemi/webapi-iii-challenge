b53edcdf5dd252e8302d483aeecc387c1fef6fa8
abidex4yemi <abidex4yemi@gmail.com>
2019-07-10 18:11:11 +0100

    Create error handler helpers

---
+++ src/server/helpers/error/errorTypes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Define error constants
+ */
+export const errorTypes = {
+	BAD_REQUEST: 400,
+	CONFLICT: 409,
+	NOT_FOUND: 404,
+	CREATED: 201,
+	OK: 200,
+	GENERIC_ERROR: 500
+};
---
+++ src/server/helpers/index.js
@@ -0,0 +1,5 @@
+/**
+ * Export all from helpers
+ */
+export { createError } from './error/createError';
+export { errorTypes } from './error/errorTypes';
