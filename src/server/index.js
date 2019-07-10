a160f8870aed7bc4bc5e6b6b44db2cc5ab3d9cd5
abidex4yemi <abidex4yemi@gmail.com>
2019-07-10 17:41:28 +0100

    Set up application main file (index.js)

---
+++ src/server/index.js
@@ -0,0 +1,41 @@
+/**
+ * Application entry point
+ */
+'use strict';
+
+/**
+ * Module dependencies
+ */
+import express from 'express';
+import logger from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+/**
+ * Module constants
+ */
+// Get port number if set or default to 2019
+const PORT = process.env.PORT || 2019;
+
+// Initialize express app
+const app = express();
+
+/**
+ * Set up middleware
+ */
+
+// Parse application/json
+app.use(express.json());
+
+// allow any request from a different domain
+// cross origin resource sharing (cors)
+app.use(cors());
+
+// Log all http request
+app.use(logger());
+
+// Set http headers for security reasons
+app.use(helmet());
+
+// Start application on port 2019
+app.listen(PORT, console.log(`Server running on port: ${PORT}`));
